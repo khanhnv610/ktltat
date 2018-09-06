672d2d307dfc92733c0597093cc3495534e5941b
khanhnv610 <32123259+khanhnv610@users.noreply.github.com>
2018-09-06 11:03:10 +0700

    Lab 1.1

---
+++ Lab1.1/lab1.1.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+using namespace std;
+const int inputSize=10;
+string inputString();
+void copyStrs(string, char[] );
+void subStrs(char[],char[]);
+void getStrs(int,int,char[],char[]);
+
+int main()
+{
+    char strs[inputSize];
+    char sub[inputSize];
+    string s1 = inputString();
+    copyStrs(s1,strs);
+    //cout<< "string copy : " <<strs;
+    subStrs(strs,sub);
+    cout << "sub string: " << sub << endl;
+    return 0;
+}
+
+string inputString()
+{
+    cout << "Type a string: ";
+    string s;
+    getline(cin,s);
+    return s;
+}
+
+void copyStrs(string s, char strs[])
+{
+    int i=0;
+    int length = s.length()>inputSize?inputSize:s.length();
+    for (i = 0; i < length-1; i++)
+        strs[i] = s.at(i);
+    strs[i] ='\0';
+}
+
+void subStrs(char strs[],char newStrs[])
+{
+    int start;
+    int end;
+    cout << "Starting point: ";
+    cin >> start;
+    cout << "Ending point: ";
+    cin >> end;
+    getStrs(start,end,strs,newStrs);
+}
+
+void getStrs(int start,int end,char strs[],char newStrs[])
+{
+    int len = end-start+1;
+    if(len>inputSize)
+    {
+        len = inputSize;
+        cout<< "\nchuoi sau khi cat toi da 9 ky tu\n";
+    }
+    int i=0;
+    for (i=0; i < len - 1; i++)
+        newStrs[i] = strs[start + i];
+    newStrs[i] = '\0';
+}
